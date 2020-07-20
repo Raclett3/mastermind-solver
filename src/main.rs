0d788e48e8a662eb5bfb265d9df69888884db74d
Raclett3 <im@s283.net>
2020-07-20 10:52:06 +0900

    Add prompts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::stdin;
+
 fn main() {
     println!("Mastermind solver");
+    let colors = loop {
+        let mut buffer = String::new();
+        println!("How many colors will you use:");
+        stdin().read_line(&mut buffer).unwrap();
+        let length = buffer.len();
+        if let Ok(colors) = buffer[0..length - 1].parse::<usize>() {
+            break colors;
+        }
+    };
+    let repitition = loop {
+        let mut buffer = String::new();
+        println!("Do you allow repetition?(y/N):");
+        stdin().read_line(&mut buffer).unwrap();
+        break match buffer.chars().next().unwrap() {
+            'Y' | 'y' => true,
+            'N' | 'n' | '\n' => false,
+            _ => continue,
+        };
+    };
+    println!("{}", colors);
+    println!("{}", repitition);
 }
