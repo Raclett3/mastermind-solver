4e3ec1869ec4703fc0a233b4ac3916db23cd266a
Raclett3 <im@s283.net>
2020-07-20 11:06:34 +0900

    Add prompt to ask the length of the answer

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use std::io::stdin;
 
-fn main() {
-    println!("Mastermind solver");
-    let colors = loop {
+fn prompt_usize(prompt: &str) -> usize {
+    loop {
         let mut buffer = String::new();
-        println!("How many colors will you use:");
+        println!("{}", prompt);
         stdin().read_line(&mut buffer).unwrap();
         let length = buffer.len();
         if let Ok(colors) = buffer[0..length - 1].parse::<usize>() {
-            break colors;
+            return colors;
         }
-    };
+    }
+}
+
+fn main() {
+    println!("Mastermind solver");
+    let colors = prompt_usize("How many kinds of colors does answer have?:");
+    let answer_len = prompt_usize("How many colors does answer have?:");
     let repetition = loop {
         let mut buffer = String::new();
         println!("Do you allow repetition?(y/N):");
@@ -21,6 +26,11 @@ fn main() {
             _ => continue,
         };
     };
+    if answer_len > colors && !repetition {
+        eprintln!("Length of the answer should be larger than kinds of colors if the answer has no color repetition");
+        std::process::exit(1);
+    }
     println!("{}", colors);
+    println!("{}", answer_len);
     println!("{}", repetition);
 }
